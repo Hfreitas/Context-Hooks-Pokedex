2725f83e220f10bee347785ffaba67c8efe13990
Hfreitas <hfreitas86@gmail.com>
2020-07-12 23:26:53 -0300

    Provider included and pokeAPI file modified

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { PokedexProvider as Provider } from './context';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
--- src/services/pokeApi.js
+++ src/services/pokeApi.js
@@ -1,5 +1,5 @@
-export const fetchPokemonList = async (limit, offset) => {
-  const url = `https://pokeapi.com/api/v2/pokemon?limit=${limit}&offset=${offset}`;
+export const fetchPokemonList = async (offset, limit) => {
+  const url = `https://pokeapi.com/api/v2/pokemon?offset=${offset}&limit=${limit}`;
   const response = await fetch(url);
   const json = await response.json();
   return response.ok ? Promise.resolve(json) : Promise.rejects(json);
