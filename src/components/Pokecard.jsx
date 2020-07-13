96aa53310f348b506da3807402c8cd9f63ca3572
Hfreitas <hfreitas86@gmail.com>
2020-07-13 00:43:18 -0300

    pokemoncard component created

---
+++ src/components/Pokecard.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Pokecard = ({ item }) => {
+  const { status, value } = item;
+
+  if (status === 'rejected') {
+    return (
+      <div>
+        <p>{value}</p>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <header>
+        <img
+          src={value.sprites.front_default}
+          alt={`${value.name} frontal vision`}
+        />
+        <img
+          src={value.sprites.back_default}
+          alt={`${value.name} back vision`}
+        />
+      </header>
+      <div>
+        <p>`Nr. ${value.id}`</p>
+        {value.types.map(({ type: { name } }) => (
+          <span key={name}>{name}</span>
+        ))}
+      </div>
+      <footer>{value.name}</footer>
+    </div>
+  );
+};
+
+Pokecard.propTypes = {
+  item: PropTypes.shape({
+    status: PropTypes.string,
+    value(value) {
+      return typeof value !== 'string' || typeof value !== 'object'
+        ? new Error(
+            `Invalid prop value supplied to Pokecard. Validation failed`,
+          )
+        : null;
+    },
+  }).isRequired,
+};
+
+export default Pokecard;
