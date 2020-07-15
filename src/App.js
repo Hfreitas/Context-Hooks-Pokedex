2942d4fa75e712d1908ad46b6b26ee431ce85f92
Hfreitas <hfreitas86@gmail.com>
2020-07-15 19:42:38 -0300

    updating stules page paths

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Home, Details, NotFound } from './pages';
-import './App.css';
+import './styles/App.css';
 
 function App() {
   return (
