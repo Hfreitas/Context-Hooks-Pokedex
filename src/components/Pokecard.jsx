64096770294e81913f29d352ccefe357fe7fab0e
Hfreitas <hfreitas86@gmail.com>
2020-07-15 19:33:19 -0300

    link to details page added to pokecard

--- src/components/Pokecard.jsx
+++ src/components/Pokecard.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 const Pokecard = ({ item }) => {
@@ -13,25 +14,27 @@ const Pokecard = ({ item }) => {
   }
 
   return (
-    <div>
-      <header>
-        <img
-          src={value.sprites.front_default}
-          alt={`${value.name} frontal vision`}
-        />
-        <img
-          src={value.sprites.back_default}
-          alt={`${value.name} back vision`}
-        />
-      </header>
-      <div>
-        <p>{`Nr. ${value.id}`}</p>
-        {value.types.map(({ type: { name } }) => (
-          <span key={name}>{name}</span>
-        ))}
-      </div>
-      <footer>{value.name}</footer>
-    </div>
+    <section>
+      <Link to={`pokemon/${value.name}`}>
+        <header>
+          <img
+            src={value.sprites.front_default}
+            alt={`${value.name} frontal vision`}
+          />
+          <img
+            src={value.sprites.back_default}
+            alt={`${value.name} back vision`}
+          />
+        </header>
+        <div>
+          <p>{`Nr. ${value.id}`}</p>
+          {value.types.map(({ type: { name } }) => (
+            <span key={name}>{name}</span>
+          ))}
+        </div>
+        <footer>{value.name}</footer>
+      </Link>
+    </section>
   );
 };
 
