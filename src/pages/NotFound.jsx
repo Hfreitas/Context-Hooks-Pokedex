c70b90f0fd8bc6eeb09a53f21bba9d526e1ca9fa
Hfreitas <hfreitas86@gmail.com>
2020-07-15 19:33:00 -0300

    page not found component created

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MissigNo from '../assets/missigno.webp';
+
+const NotFound = () => {
+  return (
+    <section>
+      <img src={MissigNo} alt="Wild MissigNo Appears!" />
+    </section>
+  );
+};
+
+export default NotFound;
