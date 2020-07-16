8765af2f152a297985bbe604bc39654af11214df
Hfreitas <hfreitas86@gmail.com>
2020-07-15 23:08:51 -0300

    adding new loading component

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as PaginationComponent } from './PaginationComponent';
 export { default as Pokecard } from './Pokecard';
 export { default as PokeGrid } from './PokeGrid';
 export { default as SearchBar } from './SearchBar';
+export { default as Loading } from './Loading';
