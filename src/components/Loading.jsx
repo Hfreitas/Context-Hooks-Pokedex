2da9fec69af2f513ad286a6e7ee89acfaeed97dd
Hfreitas <hfreitas86@gmail.com>
2020-07-15 23:05:50 -0300

    adding Loading component

---
+++ src/components/Loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Pikachu from '../assets/pikachu.gif';
+
+const Loading = () => {
+  return (
+    <div>
+      <img src={Pikachu} alt="running pikachu" />
+    </div>
+  );
+};
+
+export default Loading;
