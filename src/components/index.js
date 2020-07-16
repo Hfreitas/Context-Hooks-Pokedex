01f1bd73573bc5c7b7722280587e06935eb92885
Hfreitas <hfreitas86@gmail.com>
2020-07-15 22:18:16 -0300

    improving import paths

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Header } from './Header';
+export { default as PaginationComponent } from './PaginationComponent';
+export { default as Pokecard } from './Pokecard';
+export { default as PokeGrid } from './PokeGrid';
+export { default as SearchBar } from './SearchBar';
