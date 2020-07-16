ac9ab7549df9ea1d79cccedeff00983e5b911859
Hfreitas <hfreitas86@gmail.com>
2020-07-15 23:50:19 -0300

    corrected links order and images

--- src/components/PaginationComponent.jsx
+++ src/components/PaginationComponent.jsx
@@ -2,19 +2,35 @@ import React, { useContext } from 'react';
 import { PokedexContext } from '../context';
 
 const PaginationComponent = () => {
-  const { controlPagination } = useContext(PokedexContext);
+  const { offset, controlPagination } = useContext(PokedexContext);
   return (
     <div>
-      <button type="button" onClick={() => controlPagination.forward}>
-        <i className="fas fa-step-forward" />
-      </button>
-      <button type="button" onClick={() => controlPagination.backward}>
-        <i className="fas fa-backward" />
-      </button>
-      <button type="button" onClick={() => controlPagination.first}>
+      <button
+        type="button"
+        onClick={controlPagination.first}
+        disabled={offset === 0}
+      >
         <i className="fas fa-fast-backward" />
       </button>
-      <button type="button" onClick={() => controlPagination.last}>
+      <button
+        type="button"
+        onClick={controlPagination.backward}
+        disabled={offset === 0}
+      >
+        <i className="fas fa-step-backward" />
+      </button>
+      <button
+        type="button"
+        onClick={controlPagination.forward}
+        disabled={offset === 960}
+      >
+        <i className="fas fa-step-forward" />
+      </button>
+      <button
+        type="button"
+        onClick={controlPagination.last}
+        disabled={offset === 960}
+      >
         <i className="fas fa-fast-forward" />
       </button>
     </div>
