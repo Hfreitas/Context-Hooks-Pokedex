d24a85547169222dd3f7b6efe23da5e4462e5f79
Hfreitas <hfreitas86@gmail.com>
2020-07-15 21:47:40 -0300

    adding methods to context

--- src/context/index.js
+++ src/context/index.js
@@ -1,21 +1,38 @@
 import React, { createContext, useState } from 'react';
 import PropTypes from 'prop-types';
 import { fetchPokemonList, fetchPokemonListDetails } from '../services/pokeApi';
-import useCreateDataList from '../hooks/useCreateDataList';
+import useCreateData from '../hooks/useCreateData';
 
 export const PokedexContext = createContext();
 
 export function PokedexProvider({ children }) {
   const [offset, setOffset] = useState(0);
   const [limit, setLimit] = useState(20);
-  const pokelist = useCreateDataList(
+  const [redirect, setRedirect] = useState(false);
+  const [value, setValue] = useState('');
+  const pokelist = useCreateData(
     offset,
     limit,
     fetchPokemonList,
     fetchPokemonListDetails,
   );
 
-  const context = { setOffset, setLimit, pokelist };
+  const controlPagination = {
+    forward: () => setOffset((prevOffset) => prevOffset.offset + limit),
+    backward: () => setOffset((prevOffset) => prevOffset.offset - limit),
+    first: () => setOffset(0),
+    last: () => setOffset(960),
+  };
+
+  const context = {
+    controlPagination,
+    setLimit,
+    pokelist,
+    redirect,
+    setRedirect,
+    value,
+    setValue,
+  };
   return (
     <div>
       <PokedexContext.Provider value={context}>
