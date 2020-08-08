30af87e566c33210391105752bcacb8ef2e71d92
Hfreitas <hfreitas86@gmail.com>
2020-08-07 23:12:38 -0300

    fetch pokemon data hook ok

---
+++ src/hooks/useFetchPokemonData.js
@@ -0,0 +1,51 @@
+import { useCallback, useEffect, useState } from 'react';
+
+const pokeData = {
+  sprite: '',
+  nationalDexNumber: null,
+  name: '',
+  types: [],
+  height: null,
+  weight: null,
+};
+
+const setPokeData = (data) => ({
+  ...pokeData,
+  sprite: data.sprites.other['official - artwork'].front_default,
+  nationalDexNumber: data.id,
+  name: data.name,
+  types: [...data.types],
+  height: data.height,
+  weight: data.weight,
+});
+
+export default function useFetchPokemonData(callback, param) {
+  const [pokemon, setPokemon] = useState(pokeData);
+  const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState('');
+
+  const getPokeData = useCallback(() => {
+    callback(param).then(
+      (response) => {
+        setPokemon(setPokeData(response));
+        setIsFetching(false);
+      },
+      (response) => {
+        setError(response.message);
+        setIsFetching(false);
+      },
+    );
+  }, [param]);
+
+  useEffect(() => {
+    setIsFetching(true);
+    getPokeData();
+    return () => {
+      setIsFetching(false);
+      setPokemon(pokeData);
+      setError('');
+    };
+  }, [getPokeData]);
+
+  return { pokemon, isFetching, error };
+}
