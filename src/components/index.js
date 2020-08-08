5cef05e9c0ea892a1913cd2227b2e3dbd0b24f0f
Hfreitas <hfreitas86@gmail.com>
2020-08-07 23:45:57 -0300

    adjustment for more easy imports

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export { default as Pokecard } from './Pokecard';
 export { default as PokeGrid } from './PokeGrid';
 export { default as SearchBar } from './SearchBar';
 export { default as Loading } from './Loading';
+export { default as PokeContainer } from './PokeContainer';
